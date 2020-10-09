7418dd52dee117b40c22d6b28e178ae9a6283072
Alexandru-Nicolae DUNĂRENŢU (102451) <alexandru.dunarentu@stud.etti.upb.ro>
2020-10-09 14:46:57 +0200

    Updates

---
+++ precache-manifest.9664c0a40e792921db678fbec7603fd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da4b6fb61e8b47ae1805fd5ca3cf17f",
+    "url": "/tinder-clone/index.html"
+  },
+  {
+    "revision": "e63e49685084cfb7d57a",
+    "url": "/tinder-clone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9630cc08ed8f374e24fd",
+    "url": "/tinder-clone/static/js/2.f50228d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tinder-clone/static/js/2.f50228d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e63e49685084cfb7d57a",
+    "url": "/tinder-clone/static/js/main.a8eccb0e.chunk.js"
+  },
+  {
+    "revision": "de86c0115f6746d23e21",
+    "url": "/tinder-clone/static/js/runtime-main.acd27e62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tinder-clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
